76ed162e9f72bf9c0142a508e57eb770303ca28b
fuw4kr <kristinazakharchenko19@gmail.com>
2025-11-29 18:05:27 +0200

    Add POST support for alerts

--- myServer/src/controllers/AlertController.cpp
+++ myServer/src/controllers/AlertController.cpp
@@ -1,5 +1,32 @@
 #include "AlertController.h"
+#include <drogon/drogon.h>
+#include <json/json.h>
 #include "../dto/EntityDtos.h"
+
+namespace
+{
+const std::vector<std::string>& alertCreateColumns()
+{
+    static const std::vector<std::string> cols = {
+        "id",
+        "event_id",
+        "alert_type",
+        "message",
+        "severity",
+        "resolved",
+        "created_at"};
+    return cols;
+}
+
+drogon::HttpResponsePtr makeErrorResponse(drogon::HttpStatusCode code, const std::string& message)
+{
+    Json::Value body;
+    body["error"] = message;
+    auto resp = drogon::HttpResponse::newHttpJsonResponse(body);
+    resp->setStatusCode(code);
+    return resp;
+}
+} // namespace
 
 AlertController::AlertController(const drogon::orm::DbClientPtr& db)
     : TableControllerBase(db,
@@ -15,4 +42,54 @@ void AlertController::getAlerts(
     std::function<void(const drogon::HttpResponsePtr&)>&& callback)
 {
     fetchTable(req, std::move(callback));
+}
+
+void AlertController::createAlert(
+    const drogon::HttpRequestPtr& req,
+    std::function<void(const drogon::HttpResponsePtr&)>&& callback)
+{
+    auto json = req->getJsonObject();
+    if (!json || !json->isObject())
+    {
+        callback(makeErrorResponse(drogon::k400BadRequest, "Body must be a JSON object"));
+        return;
+    }
+
+    auto cb = std::move(callback);
+    auto onSuccess = [cb, repo = tableRepository_](const drogon::orm::Result& res) mutable
+    {
+        if (res.empty())
+        {
+            cb(makeErrorResponse(drogon::k500InternalServerError, "Insert returned no data"));
+            return;
+        }
+
+        auto resp = drogon::HttpResponse::newHttpJsonResponse(repo->toJsonObject(res));
+        resp->setStatusCode(drogon::k201Created);
+        cb(resp);
+    };
+
+    auto onError = [cb](const std::exception_ptr& ex) mutable
+    {
+        std::string msg = "Database error";
+        try
+        {
+            if (ex)
+                std::rethrow_exception(ex);
+        }
+        catch (const std::exception& e)
+        {
+            msg = e.what();
+        }
+        cb(makeErrorResponse(drogon::k500InternalServerError, msg));
+    };
+
+    try
+    {
+        tableRepository_->insert(*json, alertCreateColumns(), onSuccess, onError);
+    }
+    catch (const std::exception& ex)
+    {
+        cb(makeErrorResponse(drogon::k400BadRequest, ex.what()));
+    }
 }
